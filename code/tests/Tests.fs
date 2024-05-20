17de2d5d8c0c2f79f2a350fc9695886db11321af
M Batsaikhan <mb34@williams.edu>
2024-05-19 23:54:23 -0400

    late changes

--- code/tests/Tests.fs
+++ code/tests/Tests.fs
@@ -66,40 +66,51 @@ type TestEvaluator () =
     // test to see if 'getUniqueVariableList' function is working correctly
     [<TestMethod>]
     member this.TestMethod4 () = 
-            let input = [Activation (Protein "A", Gene "b");
-                                        Activation (Gene "b", Phenotype "*c")]
-            let result = getUniqueVariableList input
-            let expected = [Protein "A"; Gene "b"; Phenotype "*c"]
-            Assert.AreEqual(result, expected)
+        let input = [Activation (Protein "A", Gene "b");
+                                    Activation (Gene "b", Phenotype "*c")]
+        let result = getUniqueVariableList input
+        let expected = [Protein "A"; Gene "b"; Phenotype "*c"]
+        Assert.AreEqual(result, expected)
     
     // test to see if 'initializeMatrix' function is working correctly
     [<TestMethod>]
     member this.TestMethod5 () = 
-            let input = [Activation (Protein "A", Gene "b");
-                                        Activation (Gene "b", Phenotype "*c")]
-            let result = initializeMatrix input
-            let x = array2D [[|double(0); 1; 0|]; [|0; 0; 1|]; [|0; 0; 0|]]
-            let expected = Matrix<double>.Build.DenseOfArray(x)
-            Assert.AreEqual(result, expected)
+        let input = [Activation (Protein "A", Gene "b");
+                                    Activation (Gene "b", Phenotype "*c")]
+        let result = initializeMatrix input
+        let x = array2D [[|double(0); 1; 0|]; [|0; 0; 1|]; [|0; 0; 0|]]
+        let expected = Matrix<double>.Build.DenseOfArray(x)
+        Assert.AreEqual(result, expected)
 
     // test to see if 'getMatrixDerivatives' function is working correctly
     [<TestMethod>]
     member this.TestMethod6 () = 
-            let x = array2D [[|double(0); 1; 0|]; [|0; 0; 1|]; [|0; 0; 0|]]
-            let input = Matrix<double>.Build.DenseOfArray(x)
-            let result = getMatrixDerivatives input 0
-            
-            let y = array2D [[|double(0); 1; 1|]; [|0; 0; 1|]; [|0; 0; 0|]]
-            let expected = Matrix<double>.Build.DenseOfArray(y)
-            Assert.AreEqual(result, expected)
+        let x = array2D [[|double(0); 1; 0|]; [|0; 0; 1|]; [|0; 0; 0|]]
+        let input = Matrix<double>.Build.DenseOfArray(x)
+        let result = getMatrixDerivatives input 0
+        
+        let y = array2D [[|double(0); 1; 1|]; [|0; 0; 1|]; [|0; 0; 0|]]
+        let expected = Matrix<double>.Build.DenseOfArray(y)
+        Assert.AreEqual(result, expected)
 
-    // test to see if 'getMatrixDerivatives' function is working correctly
+    // test to see if 'eval' function is working correctly
     [<TestMethod>]
     member this.TestMethod7 () = 
-            let x = array2D [[|double(0); 1; 0|]; [|0; 0; 1|]; [|0; 0; 0|]]
-            let input = Matrix<double>.Build.DenseOfArray(x)
-            let result = getMatrixDerivatives input 0
-            
-            let y = array2D [[|double(0); 1; 1|]; [|0; 0; 1|]; [|0; 0; 0|]]
-            let expected = Matrix<double>.Build.DenseOfArray(y)
-            Assert.AreEqual(result, expected)
+        let input = [Activation (Protein "A", Gene "b");
+                                    Inhibition (Gene "b", Phenotype "*c")]
+        let result = eval input
+
+        let expected = [Inhibition (Protein "A", Phenotype "*c")]
+        Assert.AreEqual(result, expected)
+
+    // (attempt) test to see if 'testQuery' function is working properly
+    [<TestMethod>]
+    member this.TestMethod9 () = 
+        let q = Query (Phenotype "*c", Protein "A")
+        let seq = [Activation (Protein "A", Gene "b");
+                                    Inhibition (Gene "b", Phenotype "*c");
+                                    Query (Phenotype "*c", Protein "A")]
+        let m = array2D [[|double(0); 1; 0|]; [|0; 0; 2|]; [|0; 0; 0|]]
+        let M = Matrix<double>.Build.DenseOfArray(m)
+        let result = testQuery q seq M
+        Assert.IsTrue(true)
